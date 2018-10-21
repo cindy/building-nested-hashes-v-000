8ba7221b199e531c627f6777d6aa3c21cc2bb10b
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-21 00:20:51 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,16 +1,28 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
+    :montague => {
       :patriarch => {
-        name: '', age: ''
-        },
+        :name => "Lord Montague",
+        :age => "53"
+    },
       :matriarch => {
-        name: '', age: ''
-        },
+        :name => "Lady Montague",
+        :age => "54"
+    },
       :hero => {
-        name: '', age: '', status: ''
+        :name => "Romeo",
+        :age => "15"
+    },
+      :hero_friends => [
+        {
+           :name => "Benvolio",
+           :age => "17",
+           :attitude => "worried"
         },
-      :hero_friends => []
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
    },
    :capulet => {
       :patriarch => {
