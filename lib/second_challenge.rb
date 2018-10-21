39d3aee29cacad9c565196bff213bf6a10c0092e
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-21 00:14:41 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,11 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      patriarch: {}, matriarch: {}, :hero => {}, :friends => {}
       },
     :capulet => {
-      #your key/value pairs here!
-    },
-    :hero => {}, :friends => {}}
+      patriarch: {}, matriarch: {}, :hero => {}, :friends => {}
+    }
 
 
 end
