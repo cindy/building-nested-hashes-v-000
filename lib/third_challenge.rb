3b8534777bb8aa45a72a19920b072bc0389f801e
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-21 00:25:28 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -23,7 +23,7 @@ def third_challenge
           :name => "Mercutio",
           :age => "18",
           :attitude => "hot-headed"
-   }},
+   }]},
    :capulet => {
       :patriarch => {
         name: '', age: ''
